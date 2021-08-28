90e5c936d81e80368a5dc153e3b2c325f8577b75
crmills <chris.mills0905@gmail.com>
2021-08-28 16:54:47 -0400

    initial commit of the repository

---
+++ HelpDestDotNet/HelpDestDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelpDestDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
